c35f18ecd33f76adc24365a66de34842ccd1d3d5
hoo29 <huwmcnamara@msn.com>
2020-12-02 20:06:03 +0000

    first commit

---
+++ 1/main.py
@@ -0,0 +1,55 @@
+from typing import List
+import time
+
+max_value = 2020
+
+
+def find_value1(expenses: List[int]):
+    for ind1, val1 in enumerate(expenses):
+        for val2 in expenses[ind1 + 1:]:
+            if val1 + val2 > max_value:
+                break
+            if val1 + val2 == max_value:
+                return val1 * val2
+
+
+def p1(expenses: List[int]):
+    value = find_value1(expenses)
+    print(value)
+
+
+def find_value2(expenses: List[int]):
+    for ind1, val1 in enumerate(expenses):
+        for ind2, val2 in enumerate(expenses[ind1 + 1:]):
+            if val1 + val2 > max_value:
+                break
+            for val3 in expenses[ind2 + 1:]:
+                if val1 + val2 + val3 == max_value:
+                    return val1 * val2 * val3
+                if val1 + val2 + val3 > max_value:
+                    break
+
+
+def p2(expenses: List[int]):
+    value = find_value2(expenses)
+    print(value)
+
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+
+    with open('./1/input.txt') as f:
+        expenses = f.readlines()
+
+    # take off \n
+    expenses = [int(x.rstrip()) for x in expenses]
+    # remove values that will never work
+    expenses = [x for x in expenses if x + min(expenses) <= max_value]
+    # sort for early break out
+    expenses.sort()
+
+    p1(expenses)
+    p2(expenses)
+
+    end = time.perf_counter()
+    print((end - start) * 1000)
---
+++ 2/main.py
@@ -0,0 +1,56 @@
+from typing import List
+import time
+
+
+def p1(passwords: List[str]):
+    matches = 0
+    for item in passwords:
+        parts = item.split(' ')
+        req = parts[0]
+        char = parts[1][:-1]
+        password = parts[2]
+
+        min_items = int(req.split('-')[0])
+        max_items = int(req.split('-')[1])
+
+        count = password.count(char)
+        if count >= min_items and count <= max_items:
+            matches += 1
+
+    print(matches)
+
+
+def p2(passwords: List[str]):
+    matches = 0
+    for item in passwords:
+        parts = item.split(' ')
+        req = parts[0]
+        char = parts[1][:-1]
+        password = parts[2]
+
+        pos1 = int(req.split('-')[0])
+        pos2 = int(req.split('-')[1])
+
+        match1 = password[pos1 - 1] == char
+        match2 = password[pos2 - 1] == char
+
+        if match1 ^ match2:
+            matches += 1
+
+    print(matches)
+
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+
+    with open('./2/input.txt') as f:
+        passwords = f.readlines()
+
+    # take off \n
+    passwords = [x.rstrip() for x in passwords]
+
+    p1(passwords)
+    p2(passwords)
+
+    end = time.perf_counter()
+    print((end - start) * 1000)
